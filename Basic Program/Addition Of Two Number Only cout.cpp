d772d8cd90a6ad8d959f811abf86880690b5c037
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-22 21:49:58 +0530

    Add files via upload

---
+++ Basic Program/Addition Of Two Number Only cout.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int No1 = 10 , No2 = 20 , Sum = 0;
+    
+    Sum = No1 + No2;
+    
+    cout << endl <<" Addition of "<< No1<< " & " << No2 << " is = " << Sum << " . " <<endl;
+     
+    cout << endl << " Thanks.....";
+    
+    getch();
+    
+    return 0;
+}
