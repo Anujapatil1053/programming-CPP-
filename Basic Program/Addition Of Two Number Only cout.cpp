6bb5122e1e5b9e4c24ae63f770aa909208410c7f
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-09-01 13:38:10 +0530

    Update Addition Of Two Number Only cout.cpp

--- Basic Program/Addition Of Two Number Only cout.cpp
+++ Basic Program/Addition Of Two Number Only cout.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-    int No1 = 10 , No2 = 20 , Sum = 0;
-    
-    Sum = No1 + No2;
-    
-    cout << endl <<" Addition of "<< No1<< " & " << No2 << " is = " << Sum << " . " <<endl;
-     
-    cout << endl << " Thanks.....";
-    
-    getch();
-    
-    return 0;
+                int No1 = 10 , No2 = 20 , Sum = 0;
+
+                Sum = No1 + No2;
+
+                cout << endl <<" Addition of "<< No1<< " & " << No2 << " is = " << Sum << " . " <<endl;
+
+                cout << endl << " Thanks.....";
+
+                getch();
+
+                return 0;
 }
