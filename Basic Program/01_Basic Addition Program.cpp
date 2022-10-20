60a08926e303cd07879af4fc662ee4d703f94aaf
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-10-20 20:29:38 +0530

    Add files via upload

---
+++ Basic Program/01_Basic Addition Program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int iNo1 = 15, iNo2 = 55, iAns = 0;
+    
+    iAns = iNo1 + iNo2;
+    
+    cout << endl << "Addition Is : " << iAns << endl ;
+    
+    getch();
+    return 0;
+}
