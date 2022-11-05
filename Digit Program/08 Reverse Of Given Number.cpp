7b45d364da6b367fa0051151e0917acfc2b7d6c3
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-11-05 08:59:45 +0530

    Add files via upload

---
+++ Digit Program/08 Reverse Of Given Number.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Dig = 0,Rev = 0,Temp = 0;
+
+    cout <<"Enter A Number ="<< endl;
+    cin >> No;
+
+    Temp = No;
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10;
+        Rev = (Rev * 10) + Dig;
+        Temp = Temp /10;
+    }
+
+    cout << "Reverse Of Given Number "<< Rev <<endl;
+
+    getch();
+    return 0;
+}
---
+++ Digit Program/09 Given Number Is Palindrom Or Not.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Dig = 0,Rev = 0,Temp = 0;
+
+    cout << "Enter A Number = "<< endl;
+    cin >> No;
+
+    Temp = No;
+
+    while(Temp > 0)
+    {
+        Dig = Temp % 10;
+        Rev = (Rev * 10) + Dig;
+        Temp = Temp /10;
+    }
+    if(No == Rev)
+    {
+        cout  << " \n\n"<< No << " Is Palindrome "<< endl;
+    }
+    else
+    {
+        cout << " \n\n "<< No << " Is Not Palindrome "<< endl;
+    }
+
+    getch();
+    return 0;
+}
