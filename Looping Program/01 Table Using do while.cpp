35d037fc62ce13f7873f2199d9e38ce30cf50165
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-11-06 17:43:22 +0530

    Add files via upload

---
+++ Looping Program/01 Table Using do while.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,i = 0;
+
+    cout << " Enter Any Number = "<< endl;
+    cin >> No;
+
+    i = 1;
+
+    do
+    {
+        cout << No << " * "<< i << " = "<< No * i <<endl;
+        i++;
+    }
+    while(i <= 10);
+
+    getch();
+    return 0;
+
+}
